7eb0834bf0fae7548144207e0c0db8b148b04a93
mcraig567 <craig.matthewt@gmail.com>
2021-04-25 12:04:23 -0400

    Added navigation buttons

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Header() {
+	return (
+		<span>
+			<button onClick={viewHome}>Home</button>
+			<button onClick={viewUser}>User</button>
+			<button onClick={viewAnimals}>Animals</button>
+			<button onClick={addSighting}>Add Sighting</button>
+		</span>
+	);
+}
+
+function App() {
+	return (
+		<div>
+			<h1>Ahmic Animals</h1>
+			<Header />
+			<h1>MAP GOES HERE</h1>
+		</div>
+	);
+}		
+
 const element = <h1>Hello, world</h1>;
 
 console.log("Running!");
 
 
+//Event handlers for Header
+function viewHome() {
+	console.log("Clicked Home");
+}
+
+function viewUser() {
+	console.log("Clicked User");
+}
+
+function viewAnimals() {
+	console.log("Clicked Animals");
+}
+
+function addSighting() {
+	console.log("Clicked Sighting");
+}
+
+
+
+
+
 ReactDOM.render(
-	element, 
+	<App />, 
 	document.getElementById('root')
 );
