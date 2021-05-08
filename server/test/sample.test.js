ea0e32814fb9e2598426553d714c4d79c96b72d9
Ben Craig <DukeBaird@users.noreply.github.com>
2021-05-08 16:18:35 -0400

    Update sample.test.js

--- server/test/sample.test.js
+++ server/test/sample.test.js
@@ -5,6 +5,7 @@ describe('Group of functions', () => {
 	test('function: testFunction', () => {
 		expect(testFunction(1,2)).toBe(3);
 		expect(testFunction(1,-1)).toBe(0);
+		expect(testFunction(1,0)).toBe(99); // this should fail
 	});
 
 	// Other functions would go here
