253fc913ecb62dc8ef1b5a5af23ca3d2e32f69ef
mcraig567 <61528501+mcraig567@users.noreply.github.com>
2021-05-10 19:37:26 -0400

    Mcraig/sight feat: Add pages and sightings model (#4)
    * Added navigation buttons
    
    * Changed styling to CSS only
    
    * Changed styling to CSS only
    
    * Add sighting list components
    
    * Can create new test sightings
    
    * Header buttons pass to App
    
    * testing SSH
    
    * Testing SSH
    
    * Moved Header to its own file
    
    * Updated variable names for number and SightingList
    
    * Move styles from css to sass
    
    * Updated styling to sass
    
    * Main page shows existing list of sightings
    
    * Add AnimalSet and seen locations
    
    * Change which page renders to homepage
    
    * Added switching between sighting and animal pages
    
    * Add home button functionality
    
    * Create new page for adding sightings
    
    * Create Sight model and test sighting
    
    * Add sightingBy field
    
    * Implement forEach, added newlines
    
    * updated function and component names
    
    * Update sight model to sightings
    
    * Removed _id from sightings model
    
    * Checked indents and commented out testing, fixed package
    
    * Reset package-lock to 4ff8bd3
    
    * Update package-lock to 0e4e211

---
+++ client/src/header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../style.sass';
+
+//Nav bar at top of page
+export class Header extends React.Component {
+	constructor(props) {
+		super(props)
+		this.showNewSighting = this.showNewSighting.bind(this);
+		this.showAnimalList = this.showAnimalList.bind(this);
+		this.goHome = this.goHome.bind(this);
+	};
+
+    goHome() {
+        this.props.viewHomepage();
+    }
+
+	showNewSighting() {
+		this.props.addSighting();
+	};
+
+    showAnimalList() {
+        this.props.viewAnimals();
+    }
+
+	render() {
+		return (
+			<span>
+				<button onClick={this.goHome}>Home</button>
+				<button onClick={viewUser}>User</button>
+				<button onClick={this.showAnimalList}>Animals</button>
+				<button onClick={this.showNewSighting}>Add Sighting</button>
+			</span>
+		);
+	};
+}
+
+//Event handlers for Header
+function viewUser() {
+	console.log("Clicked User");
+}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,213 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Header} from './header.jsx';
+import {AnimalLocations} from './animalLocations.jsx';
+import {NewSighting} from './addSighting.jsx';
 import '../style.sass';
 
-const element = <h1 className='test'>Hello, world</h1>;
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			sightings: [sight, sight1, sight2],
+			showing: "sight"
+		
+		};
+		this.viewHomepage = this.viewHomepage.bind(this);
+		this.addSighting = this.addSighting.bind(this);
+		this.viewAnimals = this.viewAnimals.bind(this);
+		this.createNewSighting = this.createNewSighting.bind(this);
+
+	}
+
+
+	renderSightings(){
+		const {sightings} = this.state;
+		if (!sightings | sightings.length < 0) return null;
+		const SATList = []; //Create list of SightingAsText elements
+		sightings.forEach(element =>
+			SATList.push(
+				<SightingAsText user={person} sighting={element} />
+			)
+		);
+
+		/* for (let i = 0; i < sightings.length; i+=1) {
+			SATList.push(
+				<SightingAsText user={person} sighting={sightings[i]} />
+			);
+		} */
+		return SATList;
+	};
+
+	viewHomepage() {
+		console.log("Clicked Home");
+		this.setState({
+			showing: "sight"
+		});
+	}
+
+	addSighting() {
+		console.log("Clicked Sighting");
+		this.setState(state => ({
+			//sightings: [...state.sightings, sight],
+			showing: "newSight"
+		}));
+	}
+
+	viewAnimals() {
+		console.log("Clicked Animals");
+		this.setState({
+			showing: "animals"
+		});
+	}
+
+
+	showAnimals() {
+		console.log("Showing animals");
+		const {sightings} = this.state;
+		if (!sightings | sightings.length < 0) return null;
+
+		//Create "set" of animals
+		const animals = {};
+		sightings.forEach(element => {
+			if (animals[element.animal]) {
+				animals[element.animal].push(element.location);
+			} else {
+				animals[element.animal] = [element.location];
+			}		
+		});
+
+		//Create elements to display from set
+		const animalList = [];		
+		for (let i in animals) {
+			animalList.push(
+			<AnimalLocations animal={i} locations={animals[i]}/>
+			);
+		}
+
+		return animalList;
+	}
+
+	newSighting() {
+		return <NewSighting onSubmit={this.createNewSighting}/>
+	}
+
+	createNewSighting(animal, location) {
+		const newSight = {
+			animal: animal,
+			location: location,
+			time: "Surprise"
+		};
+
+		//Add to sighting state and show all sightings
+		this.setState(state => ({
+			sightings: [...state.sightings, newSight],
+			showing: "sight"
+		}));
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Ahmic Animals</h1>
+				<Header addSighting={this.addSighting} viewAnimals={this.viewAnimals} viewHomepage={this.viewHomepage} />
+				<h1>MAP GOES HERE</h1>
+
+				{/* If showing = sight, render sightings */}
+				{this.state.showing === "sight" ? this.renderSightings() 
+
+				// else if showing = animals, show animals
+				: this.state.showing === "animals" ? this.showAnimals()
+
+				// else if showing = newSight, show new sighting input
+				: this.state.showing === "newSight" ? this.newSighting()
+
+				// Otherwise show null
+				: null}
+			</div>
+		);
+	};
+}		
+
+//Display user profile picture (if loaded)
+function Avatar(props) {
+	return (
+		<img className="Avatar"
+			src={props.user.avatarUrl}
+			alt={props.user.name}
+		/>		
+	);
+}
+
+//Include user's name below their profile picture
+function UserInfo(props) {
+	return (
+		<div className="UserInfo">
+			<Avatar user={props.user} />
+			<div className="UserInfo-Name">
+				{props.user.name}
+			</div>
+		</div>		
+	);
+}
+
+//SightingInfo
+function SightingInfo(props) {
+	return (
+		<div>
+			<h2>{props.sighting.animal}</h2>
+			<h4>{props.sighting.location}</h4>
+			<h4>{props.sighting.time}</h4>	
+		</div>	
+	);
+}
+
+//List version of a sighting
+function SightingAsText(props) {
+	return (
+		<div className="SightingAsText">
+			<img className="Sighting-Image"
+				src={props.sighting.img}
+				alt={props.sighting.animal}
+			/>
+			<SightingInfo sighting={props.sighting} />
+			<UserInfo user={props.user} />
+		</div>		
+	);
+}
+
+//Test Variables
+const person = {
+	name:"Matt",
+	avatarUrl:"fdsfsdf"
+};
+
+const sight = {
+	animal: "Cat",
+	location: "Kitchen",
+	time: "Now",
+	img: "./cat.jpg"
+};
+
+const sight1 = {
+	animal: "Dog",
+	location: "Outside",
+	time: "Now",
+	img: "./cat.jpg"
+};	
+
+const sight2 = {
+	animal: "Cat",
+	location: "Desk",
+	time: "Yesterday",
+	img: "./cat.jpg"
+};	
+
+
 
 console.log("Running!");
 
-
 ReactDOM.render(
-	element, 
+	<App />, 
 	document.getElementById('root')
 );
