79436ac307512ac6fcd5e51462d5a1a0f743c278
mcraig567 <craig.matthewt@gmail.com>
2021-05-10 20:47:31 -0400

    Switch to promises for db interactions

--- server/routes/sightingsController.js
+++ server/routes/sightingsController.js
@@ -3,28 +3,33 @@ const Sighting = new require('../models/Sighting.js');
 
 //Get all sightings in the database
 exports.getSightings = function(req, res) {
-    Sightings.find({}, function(err, sight) {
-        if (err) return console.log(err);
+    Sightings.find({}).then(sight => {
+        res.status(200);
         res.json(sight);
-    })
+    }).catch(err => {
+        console.log(err);
+    });
 };
 
 //Create a new sighting
 exports.addSighting = function(req, res) {
     const newSight = new Sighting(req.body);
-    newSight.save(function(err, sight) {
-        if (err) return console.log(err);
+    newSight.save().then(sight=> {
         res.json(sight);
+        res.status(201);
+    }).catch(err => {
+        if (err) return console.log(err);
     });
 };
 
 //Delete a sighting
 exports.deleteSighting = function(req, res) {
     Sightings.remove({
-        spottedBy: req.params.spottedBy,
-        animal: reqs.params.animal
-    }, function(err, sight) {
-        if (err) return console.log(err);
+        id: req.params.id
+    }).then(() => {
         res.json({message: 'Sighting successfully deleted'})
+        res.status(204);        
+    }).catch(err => {
+        if (err) return console.log(err);
     });
 }
