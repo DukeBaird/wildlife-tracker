150615ad4822f812bda070c9b2aed26da021dfca
mcraig567 <craig.matthewt@gmail.com>
2021-07-07 18:29:56 -0400

    Indentation fixes

--- client/src/animalSet.jsx
+++ client/src/animalSet.jsx
@@ -3,36 +3,36 @@ import ReactDOM from 'react-dom';
 import '../style.sass';
 
 export class AnimalSet extends React.Component {
-    constructor(props) {
-        super(props)
-        this.addLocations = this.addLocations.bind(this);
-    };
+	constructor(props) {
+		super(props)
+		this.addLocations = this.addLocations.bind(this);
+	};
 
-    addLocations() {
-        const loc = this.props.locations;
-        console.log(loc);
-        const locList = loc.map((location) =>
-            <div key={location}>{location}</div>
-        );
-        //console.log("Adding Locations")
-/*         for (let i = 0; i < this.props.locations.length; i+=1 ) {
-            loc.push(
-                <div>{this.props.locations[i]}</div>
-            );
-        }; */
+	addLocations() {
+		const loc = this.props.locations;
+		console.log(loc);
+		const locList = loc.map((location) =>
+			<div key={location}>{location}</div>
+		);
+		//console.log("Adding Locations")
+		/* for (let i = 0; i < this.props.locations.length; i+=1 ) {
+			loc.push(
+				<div>{this.props.locations[i]}</div>
+			);
+		}; */
 
-        return locList;
-    }
+		return locList;
+	}
 
-    render() {
-        return (
-            <div>
-                <h1>{this.props.animal}</h1>
-                <div>Seen at:</div>
-                <span>
-                    {this.addLocations()}
-                </span>
-            </div>
-        );
-    };
+	render() {
+		return (
+			<div>
+				<h1>{this.props.animal}</h1>
+				<div>Seen at:</div>
+				<span>
+					{this.addLocations()}
+				</span>
+			</div>
+		);
+	};
 }
