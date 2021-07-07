a74461cbb2e3694439ae81147d532bc94518ac6f
mcraig567 <61528501+mcraig567@users.noreply.github.com>
2021-07-07 12:18:22 -0400

    feat: Create button component (#10)
    * Create button component
    
    * Replaced header with button components
    
    * Add button styling
    
    * Adjusting whitespace and newlines to match other formatting
    
    * Fix: indentation issues
    
    * Add newline at end of file
    
    Co-authored-by: Ben Craig <Benjamin.d.craig@gmail.com>

--- client/src/addSighting.jsx
+++ client/src/addSighting.jsx
@@ -3,45 +3,43 @@ import ReactDOM from 'react-dom';
 import '../style.sass';
 
 export class NewSighting extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            animal: '',
-            location: '',
-            time: '',
-            image: ''
-        }
-        this.handleAnimalChange = this.handleAnimalChange.bind(this);
-        this.handleLocationChange = this.handleLocationChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+	constructor(props) {
+		super(props);
+		this.state = {
+			animal: '',
+			location: '',
+			time: '',
+			image: ''
+		}
+		this.handleAnimalChange = this.handleAnimalChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-    }
+	handleAnimalChange(event) {
+		this.setState({animal: event.target.value});
+	}
 
-    handleAnimalChange(event) {
-        this.setState({animal: event.target.value});
-    }
+	handleLocationChange(event) {
+		this.setState({location: event.target.value});
+	}
 
-    handleLocationChange(event) {
-        this.setState({location: event.target.value});
-    }
+	handleSubmit() {
+		this.props.createNewSighting(this.state.animal, this.state.location);
+	}
 
-    handleSubmit() {
-        this.props.createNewSighting(this.state.animal, this.state.location);
-    }
-
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Animal:
-                    <input type="text" value={this.state.animal} onChange={this.handleAnimalChange} />
-                </label>
-                <label>
-                    Location:
-                    <input type="text" value={this.state.location} onChange={this.handleLocationChange}/>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
-        )}
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Animal:
+					<input type="text" value={this.state.animal} onChange={this.handleAnimalChange} />
+				</label>
+				<label>
+					Location:
+					<input type="text" value={this.state.location} onChange={this.handleLocationChange}/>
+				</label>
+				<input type="submit" value="Submit"/>
+			</form>
+		)}
 };
