c136974ca79b8dd5d9371043c3107f471d169df7
mcraig567 <craig.matthewt@gmail.com>
2021-08-10 21:28:48 -0400

    lint fixes

--- server/lib/passport.js
+++ server/lib/passport.js
@@ -1,20 +1,20 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-const logger = require('../lib/logger.js');
-var crypto = require('crypto');
+/* eslint-disable no-underscore-dangle */
 
-var User = new require('../models/User.js');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const logger = require('./logger.js');
 
-module.exports = function() {
+const User = require('../models/User.js');
 
-	passport.serializeUser(function(user, done) {
-		logger.info("Serializing User");
+module.exports = function () {
+	passport.serializeUser((user, done) => {
+		logger.info('Serializing User');
 		done(null, user._id);
 	});
 
-	passport.deserializeUser(function(id, done) {
-		logger.info("Deserializing User");
-		User.findById(id, function(err, user) {
+	passport.deserializeUser((id, done) => {
+		logger.info('Deserializing User');
+		User.findById(id, (err, user) => {
 			done(err, user);
 		});
 	});
@@ -23,64 +23,65 @@ module.exports = function() {
 		usernameField: 'username',
 		passwordField: 'password',
 		passReqToCallback: true
-	}, function (req, username, password, done) {
-		process.nextTick(function() {
-			username = username.toLowerCase();
+	}, (req, username, password, done) => {
+		process.nextTick(() => {
+			const lowerUsername = username.toLowerCase();
 
-			User.findOne({ username: username }).then(exists => {
-				if (exists) {
-					logger.error("This user exists!");
-					return done(null, False);
-				} else {
-					logger.info(`creating new user: ${username}`);
+			User.findOne({ username: lowerUsername })
+				.then((exists) => {
+					if (exists) {
+						logger.error('This user exists!');
+						return done(null, false);
+					}
+
+					logger.info(`creating new user: ${lowerUsername}`);
 					const newUser = new User();
 
-					newUser.username = username;
+					newUser.username = lowerUsername;
 					newUser.firstName = req.body.firstName;
 					newUser.lastName = req.body.lastName;
-					newUser.time = new Date()
+					newUser.time = new Date();
 
 					const hashPass = newUser.generateHash(password);
 					newUser.password = hashPass;
 
-					newUser.save(err => {
+					return newUser.save((err) => {
 						if (err) {
-							logger.error("Issue saving new user");
-							throw err;
-						} else {
-							logger.info("Saved new user");
-							return done(null, newUser);
-						};
-					});
+							logger.error('Issue saving new user');
+							return err;
+						}
 
-				};
-			}).catch(err => {
-				logger.error("Unable to find user");
-				return done(err);
-			});
-		})
+						logger.info('Saved new user');
+						return done(null, newUser);
+					});
+				})
+				.catch((err) => {
+					logger.error('Unable to find user');
+					return done(err);
+				});
+		});
 	}));
 
 	passport.use('local-login', new LocalStrategy({
 		usernameField: 'username',
 		asswordField: 'password',
 		passReqToCallback: true
-	}, function (req, username, password, done) {
-		username = username.toLowerCase();
-		User.findOne({ 'username': username }).then(user => {
-			if (!user) {
-				return done(null, false);
-			}
+	}, (req, username, password, done) => {
+		const lowerUsername = username.toLowerCase();
+		User.findOne({ username: lowerUsername })
+			.then((user) => {
+				if (!user) {
+					return done(null, false);
+				}
 
-			if (!user.isValidPassword(password)) {
-				return done(null, false)
-			}
+				if (!user.isValidPassword(password)) {
+					return done(null, false);
+				}
 
-			return done(null, user);
-			
-		}).catch(err => {
-			return done(err);
-		});
-
+				return done(null, user);
+			})
+			.catch((err) => done(err));
 	}));
 };
+
+/* eslint-enable no-underscore-dangle */
