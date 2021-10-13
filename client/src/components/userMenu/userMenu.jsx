a68b9b07a8cec55ab354e02dc2e14965d5add4d7
Ben Craig <DukeBaird@users.noreply.github.com>
2021-10-13 17:33:37 -0400

    feat: cleanup Styling (#19)
    * Updated page layout
    * Media query on floating buttons

--- client/src/components/userMenu/userMenu.jsx
+++ client/src/components/userMenu/userMenu.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import userLogo from '../../images/userLogo.png';
-import addButton from '../../images/add.png';
-import historyButton from '../../images/history.png';
+
 import menuButton from '../../images/menu.png';
 import './userMenu.sass';
 
@@ -12,8 +11,7 @@ export class UserMenu extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			showSideBar: false, //Set to true when you need to debug sidebar
-			showFloatingButtons: true
+			showSideBar: false //Set to true when you need to debug sidebar
 		};
 		this.showUserSideBar = this.showUserSideBar.bind(this);
 		this.createSideBar = this.createSideBar.bind(this);
@@ -24,13 +22,11 @@ export class UserMenu extends React.Component {
 	showUserSideBar() {
 		if (this.state.showSideBar === false) {
 			this.setState({
-				showSideBar: true,
-				showFloatingButtons: false
+				showSideBar: true
 			});
 		} else {
 			this.setState({
-				showSideBar: false,
-				showFloatingButtons: true
+				showSideBar: false
 			});
 		}
 	};
@@ -87,23 +83,9 @@ export class UserMenu extends React.Component {
 	//Remove div with 'buttons'
 	hideUserSideBar() {
 		this.setState({
-			showSideBar: false,
-			showFloatingButtons: true
+			showSideBar: false
 		});
 	};
-
-	showFloatingButtons() {
-		if (this.props.user) {
-			const buttons = [];
-			
-			buttons.push(<img className="floatingLogo" src={addButton} alt="Add Sighting" onClick={this.props.addSighting}/>);
-			buttons.push(<img className="floatingLogo" src={historyButton} alt="Add Sighting" onClick={this.props.viewAnimals}/>);
-		
-			return buttons
-		}
-
-		return null
-	}
 
 	logUserOut() {
 		console.log("Logging user out");
@@ -128,12 +110,6 @@ export class UserMenu extends React.Component {
 						<img className="userLogo" src={menuButton} alt="User Logo"/>
 					</div>
 					{this.state.showSideBar === true ? this.createSideBar() : null}
-				</div>
-				<div id="rightSideSpacer"></div>
-				<div className="floatingButtons">
-					<div id="floatingContainer">
-						{this.state.showFloatingButtons === true ? this.showFloatingButtons() : null}
-					</div>
 				</div>
 			</div>
 		)
