dded5b8bde0151a92e88a505f7b9d9b7c5028ecb
mcraig567 <craig.matthewt@gmail.com>
2021-11-22 22:08:13 -0500

    create marker component to act as new google maps pins

---
+++ client/src/components/marker/marker.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './marker.sass';
+
+export class Marker extends React.Component {
+	constructor(props) {
+		super(props)
+	};
+
+
+	render() {
+		return <div className="marker"></div>
+	};
+}
